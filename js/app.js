355272f31495cc4d6e655f70c7bfb962e161642d
szymtur <szymtur@gmail.com>
2018-12-15 19:52:03 +0100

    added info caption

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 import Game from "./game.js";
+import isMobile from "./mobile.js";
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
+   let gameContainer = document.querySelector('#game_container');
+   let infoCaption = document.querySelector('#info_caption')
    let startButton = document.querySelector('#start');
+   
+   if (isMobile.any()) {
+      gameContainer.classList.add('invisible');
+      infoCaption.classList.remove('invisible');
+   }
 
-   startButton.addEventListener('click', function(){
+   startButton.addEventListener('click', function() {
       let newGame = new Game();
       
       newGame.showFurry();
       newGame.showCoin();
       newGame.startGame();
    
-      document.addEventListener('keydown', function(event){ 
+      document.addEventListener('keydown', function(event) { 
          newGame.turnFurry(event); 
       })
    })
