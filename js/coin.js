23bba7a7503d98298127616e1f558ccf98644587
starawneh <szymon.tarawneh@ringieraxelspringer.pl>
2022-02-16 12:57:04 +0100

    coin position

--- js/coin.js
+++ js/coin.js
@@ -3,9 +3,22 @@
 */
 
 class Coin {
-    constructor() {
-        this.x = Math.floor(Math.random() * 10);
-        this.y = Math.floor(Math.random() * 10);
+    constructor(position) {
+        this.position = position;
+        this.newPosition = this.execute();
+    }
+
+    execute() {
+        const notAllowed = [this.position, this.position + 1, this.position + 10, this.position - 1, this.position - 10];
+
+        let newCoinPosition;
+
+        do {
+            newCoinPosition = Math.floor(Math.random() * 100);
+
+        } while (newCoinPosition === undefined || notAllowed.includes(newCoinPosition));
+
+        return newCoinPosition;
     }
 }
 
